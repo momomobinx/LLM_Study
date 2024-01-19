a1906791a584f656fdc548d6f3bd46279a51acd1
momomobinx <binxue321@gmail.com>
2024-01-19 15:02:06 +0800

    vllm Qwen-1_8B-Chat

---
+++ infer-engine/vllm/run.sh
@@ -0,0 +1,3 @@
+sh ../../models/Qwen-1_8B-Chat-clone.sh
+
+python -m vllm.entrypoints.openai.api_server --model Qwen-1_8B-Chat
---
+++ models/Qwen-1_8B-Chat-clone.sh
@@ -0,0 +1,2 @@
+git lfs install
+git clone https://huggingface.co/Qwen/Qwen-1_8B-Chat
